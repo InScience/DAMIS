cc3f475345a6a0885eea7bc7ba41ebabd2821488
strazdas <strazdas@pov.lt>
2013-08-09 17:07:05 +0300

    Add dataset upload_to attribute. Fix DateTimeField name.

--- src/damis/models.py
+++ src/damis/models.py
@@ -7,23 +7,26 @@ class DatasetLicence(models.Model):
     short_title = models.CharField(max_length=30)
     url = models.URLField()
     summary = models.TextField()
-    updated = models.DatetimeField(auto_now=True)
-    created = models.DatetimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
 
 
 class FileFormat(models.Model):
     extension = models.CharField(max_length=10)
     description = models.TextField()
-    updated = models.DatetimeField(auto_now=True)
-    created = models.DatetimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
 
+
+def get_dataset_upload_path(self, instance, filename):
+    return '/%s/' % instance.author.username
 
 class Dataset(models.Model):
     title = models.CharField(max_length=255)
     licence = models.ForeignKey('DatasetLicence')
-    file = models.FileField()
+    file = models.FileField(upload_to=get_dataset_upload_path)
     file_format = models.ForeignKey('FileFormat')
     description = models.TextField()
     author = models.ForeignKey(User)
-    updated = models.DatetimeField(auto_now=True)
-    created = models.DatetimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
