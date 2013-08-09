c5e161053ed0bb9a6497078a18e3851368eee22e
strazdas <strazdas@pov.lt>
2013-08-09 16:45:20 +0300

    Add Dataset model.

--- src/damis/models.py
+++ src/damis/models.py
@@ -1,29 +1,29 @@
+from django.db import models
 from django.contrib.auth.models import User
-from subprocess import Popen, PIPE
 
 
-class UosisMIFVUAuthBackend(object):
-    def authenticate(self, username=None, password=None):
-        login_check_cmd = """sshpass -p '%s' ssh -t -t -o \
-StrictHostKeyChecking=no %s@uosis.mif.vu.lt 'pwd'""" % (password, username)
-        login_response = Popen(login_check_cmd, shell=True, stdout=PIPE,
-                                                            stderr=PIPE)
-        login_response.wait()
-        if not login_response.communicate()[0]:
-            return None
+class DatasetLicence(models.Model):
+    title = models.CharField(max_length=255)
+    short_title = models.CharField(max_length=30)
+    url = models.URLField()
+    summary = models.TextField()
+    updated = models.DatetimeField(auto_now=True)
+    created = models.DatetimeField(auto_now_add=True)
 
-        try:
-            user = User.objects.get(username=username)
-        except User.DoesNotExist:
-            user = User(username=username)
-            user.is_staff = False
-            user.is_superuser = False
-            user.set_unusable_password()
-            user.save()
-        return user
 
-    def get_user(self, id):
-        try:
-            return User.objects.get(id=id)
-        except User.DoesNotExist:
-            return None
+class FileFormat(models.Model):
+    extension = models.CharField(max_length=10)
+    description = models.TextField()
+    updated = models.DatetimeField(auto_now=True)
+    created = models.DatetimeField(auto_now_add=True)
+
+
+class Dataset(models.Model):
+    title = models.CharField(max_length=255)
+    licence = models.ForeignKey('DatasetLicence')
+    file = models.FileField()
+    file_format = models.ForeignKey('FileFormat')
+    description = models.TextField()
+    author = models.ForeignKey(User)
+    updated = models.DatetimeField(auto_now=True)
+    created = models.DatetimeField(auto_now_add=True)
