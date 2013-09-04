8be7639ac404f74a182d5e4a2d1ce19c53041e30
grazina <grazina.laurinaviciute@gmail.com>
2013-09-04 23:18:24 +0300

    Completed test_kmeans_grouping test.

--- src/algorithms/tests/kmeans.py
+++ src/algorithms/tests/kmeans.py
@@ -1,7 +1,33 @@
 from unittest import TestCase
 from algorithms import kmeans
+import os
+import os.path
+import shutil
+from numpy.random import seed
 
 
 class KMeansTests(TestCase):
     def test_kmeans_grouping(self):
-        pass
+        seed(2013)
+        testDir = 'kmeans_test'
+
+        if not os.path.exists(testDir):
+            os.makedirs(testDir)
+
+        kMax = int(10)
+        iFileName = testDir + '/gauss_5_groups.csv'
+        oDir = testDir + '/res'
+        tmpDir = testDir + '/tmp'
+        uKMeans = kmeans.UnsupervisedKMeans(tmpDir)
+
+
+        X = uKMeans.generateGaussData()
+        uKMeans.writeData(X, iFileName)
+
+        bestK, bestRatio = uKMeans.run(kMax, iFileName, oDir)
+
+        if os.path.exists(testDir):
+            shutil.rmtree(testDir)
+
+        self.assertEqual(bestK, 5)
+        self.assertEqual(bestRatio, 169.55817157830558)
