24a54f5fb42d29847f1d452aaa830c3964a1b2f4
grazina <grazina.laurinaviciute@gmail.com>
2013-09-05 19:21:23 +0300

    Added c45 tree construction test.

--- src/algorithms/tests/c45.py
+++ src/algorithms/tests/c45.py
@@ -1,7 +1,17 @@
 from unittest import TestCase
 from algorithms.c45 import c45
-
+from algorithms.c45 import tools
+from algorithms.tests import TEST_FILE_PATH
+from os.path import join
 
 class C45Tests(TestCase):
     def test_tree(self):
-        pass
+        learnFile = join(TEST_FILE_PATH, "vertebral_learn.data")
+        
+        learnData = tools.readData(learnFile)
+        alg = c45.C45(learnData)
+        tree = alg.constructTree()
+        tree = alg.pruneTree(tree)
+        
+        cls = tree.getClass(learnData[0])
+        self.assertEquals(2, cls)
