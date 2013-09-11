401dafcd6a4c983a0cc047c62ed42a5764a38f83
strazdas <strazdas@pov.lt>
2013-09-12 01:24:17 +0300

    Add custom templatetag to show uploaded file name. Use it in algorithm-list page.

---
+++ src/damis/templatetags/damis.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+from __future__ import division
+
+from django import template
+from os.path import split
+
+register = template.Library()
+
+
+@register.filter
+def filename(value):
+    return split(value)[1]
