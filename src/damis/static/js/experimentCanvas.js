b34226251b3027ffef83ab7b2d455f1a560d74e7
grazina <grazina.laurinaviciute@gmail.com>
2013-09-29 12:04:53 +0300

    Added partial connection and task box deletion functionality on right click.

--- src/damis/static/js/experimentCanvas.js
+++ src/damis/static/js/experimentCanvas.js
@@ -1,124 +1,154 @@
-;(function() {
-	
+;
+(function() {
+
 	window.experimentCanvas = {
-                
-            // this is the paint style for the connecting lines..
-            getConnectorPaintStyle : function() {
-                return {
-                    lineWidth:4,
-                    strokeStyle:"#deea18",
-                    joinstyle:"round",
-                    outlineColor:"#eaedef",
-                    outlineWidth:2
+
+		// this is the paint style for the connecting lines..
+		getConnectorPaintStyle: function() {
+			return {
+				lineWidth: 4,
+				strokeStyle: "#deea18",
+				joinstyle: "round",
+				outlineColor: "#eaedef",
+				outlineWidth: 2
+			}
+		},
+
+		// .. and this is the hover style. 
+		getConnectorHoverStyle: function() {
+			return {
+				lineWidth: 4,
+				strokeStyle: "#5C96BC",
+				outlineWidth: 2,
+				outlineColor: "white"
+			}
+		},
+
+		getEndpointHoverStyle: function() {
+			return {
+				fillStyle: "#5C96BC"
+			}
+		},
+
+		// the definition of source endpoints (the small blue ones)
+		getSourceEndpoint: function() {
+			return {
+				endpoint: "Dot",
+				paintStyle: {
+					strokeStyle: "#1e8151",
+					fillStyle: "transparent",
+					radius: 7,
+					lineWidth: 2
+				},
+				isSource: true,
+				connector: ["Flowchart", {
+					stub: [40, 60],
+					gap: 10,
+					cornerRadius: 5,
+					alwaysRespectStubs: true
+				}],
+				connectorStyle: this.getConnectorPaintStyle(),
+				hoverPaintStyle: this.getEndpointHoverStyle(),
+				connectorHoverStyle: this.getConnectorHoverStyle(),
+				dragOptions: {},
+				overlays: [["Label", {
+					location: [0.5, 1.5],
+					label: "Drag",
+					cssClass: "endpointSourceLabel"
+				}]]
+			}
+		},
+
+		// a source endpoint that sits at BottomCenter
+		// bottomSource : jsPlumb.extend( { anchor:"BottomCenter" }, sourceEndpoint),
+		// the definition of target endpoints (will appear when the user drags a connection) 
+		getTargetEndpoint: function() {
+			return {
+				endpoint: "Dot",
+				paintStyle: {
+					fillStyle: "#1e8151",
+					radius: 11
+				},
+				hoverPaintStyle: this.getEndpointHoverStyle(),
+				maxConnections: - 1,
+				dropOptions: {
+					hoverClass: "hover",
+					activeClass: "active-target"
+				},
+				isTarget: true,
+				overlays: [["Label", {
+					location: [0.5, - 0.5],
+					label: "Drop",
+					cssClass: "endpointTargetLabel"
+				}]]
+			}
+		},
+
+		// initialize 
+		init: function(spec) {
+
+			jsPlumb.importDefaults({
+				Container: spec.id,
+				// default drag options
+				DragOptions: {
+					cursor: 'pointer',
+					zIndex: 2000
+				},
+				// default to blue at one end and green at the other
+				EndpointStyles: [{
+					fillStyle: '#225588'
+				},
+				{
+					fillStyle: '#558822'
+				}],
+				// blue endpoints 7 px; green endpoints 11.
+				Endpoints: [["Dot", {
+					radius: 7
+				}], ["Dot", {
+					radius: 11
+				}]],
+				// the overlays to decorate each connection with.  note that the label overlay uses a function to generate the label text; in this
+				// case it returns the 'labelText' member that we set on each connection in the 'init' method below.
+				ConnectionOverlays: [["Arrow", {
+					location: 1
+				}], ["Label", {
+					location: 0.1,
+					id: "label",
+					cssClass: "aLabel"
+				}]]
+			});
+
+			init = function(connection) {
+				connection.getOverlay("label").setLabel(connection.sourceId.substring(6) + "-" + connection.targetId.substring(6));
+				connection.bind("editCompleted", function(o) {
+					if (typeof console != "undefined") console.log("connection edited. path is now ", o.path);
+				});
+			};
+
+			// listen for new connections; initialise them the same way we initialise the connections at startup.
+			jsPlumb.bind("connection", function(connInfo, originalEvent) {
+				init(connInfo.connection);
+			});
+
+			// make draggable						
+			jsPlumb.draggable(document.querySelectorAll(".task-box"), {
+				grid: [20, 20],
+				containment: "parent"
+			});
+
+			// listen for clicks on connections, and offer to delete connections on click.
+			jsPlumb.bind("click", function(conn, originalEvent) {
+				jsPlumb.detach(conn);
+			});
+
+            // delete connections on right click
+	        jsPlumb.bind("contextmenu", function(conn, originalEvent) {
+                if (conn.connector) {
+                    jsPlumb.detach(conn);
                 }
-            },
-                
-	    // .. and this is the hover style. 
-            getConnectorHoverStyle : function() {
-                return {
-                    lineWidth:4,
-                    strokeStyle:"#5C96BC",
-                    outlineWidth:2,
-                    outlineColor:"white"
-                }
-            },
-
-            getEndpointHoverStyle : function() {return{fillStyle:"#5C96BC"}},
-
-            // the definition of source endpoints (the small blue ones)
-            getSourceEndpoint : function() {
-                return {
-                    endpoint:"Dot",
-                    paintStyle:{ 
-                        strokeStyle:"#1e8151",
-                        fillStyle:"transparent",
-                        radius:7,
-                        lineWidth:2 
-                    },				
-                    isSource:true,
-                    connector:[ "Flowchart", { stub:[40, 60], gap:10, cornerRadius:5, alwaysRespectStubs:true } ],								                
-                    connectorStyle: this.getConnectorPaintStyle(),
-                    hoverPaintStyle: this.getEndpointHoverStyle(),
-                    connectorHoverStyle:this.getConnectorHoverStyle(),
-                    dragOptions:{},
-                    overlays:[
-                        [ "Label", { location:[0.5, 1.5], label:"Drag", cssClass:"endpointSourceLabel" } ]
-                    ]
-                }
-            },
-
-            // a source endpoint that sits at BottomCenter
-            // bottomSource : jsPlumb.extend( { anchor:"BottomCenter" }, sourceEndpoint),
-                        
-            // the definition of target endpoints (will appear when the user drags a connection) 
-            getTargetEndpoint : function() {
-                return {
-                    endpoint:"Dot",					
-                    paintStyle:{ fillStyle:"#1e8151",radius:11 },
-                    hoverPaintStyle:this.getEndpointHoverStyle(),
-                    maxConnections:-1,
-                    dropOptions:{ hoverClass:"hover", activeClass:"active-target" },
-                    isTarget:true,			
-                    overlays:[
-                        [ "Label", { location:[0.5, -0.5], label:"Drop", cssClass:"endpointTargetLabel" } ]
-                    ]
-                }
-            },			
-
-            // initialize 
-            init : function(spec) {
-				
-                jsPlumb.importDefaults({
-                    Container: spec.id,
-                    // default drag options
-                    DragOptions : { cursor: 'pointer', zIndex:2000 },
-                    // default to blue at one end and green at the other
-                    EndpointStyles : [{ fillStyle:'#225588' }, { fillStyle:'#558822' }],
-                    // blue endpoints 7 px; green endpoints 11.
-                    Endpoints : [ [ "Dot", {radius:7} ], [ "Dot", { radius:11 } ]],
-                    // the overlays to decorate each connection with.  note that the label overlay uses a function to generate the label text; in this
-                    // case it returns the 'labelText' member that we set on each connection in the 'init' method below.
-                    ConnectionOverlays : [
-                        [ "Arrow", { location:1 } ],
-                        [ "Label", { 
-                            location:0.1,
-                            id:"label",
-                            cssClass:"aLabel"
-                        }]
-                    ]
-                });		
-
-	        init = function(connection) {
-                    connection.getOverlay("label").setLabel(connection.sourceId.substring(6) + "-" + connection.targetId.substring(6));
-                    connection.bind("editCompleted", function(o) {
-                    if (typeof console != "undefined")
-                        console.log("connection edited. path is now ", o.path);
-                    });
-                };			
-
-                // listen for new connections; initialise them the same way we initialise the connections at startup.
-                jsPlumb.bind("connection", function(connInfo, originalEvent) { 
-                    init(connInfo.connection);
-                });			
-						
-	        // make draggable						
-		jsPlumb.draggable(document.querySelectorAll(".task-box"), { grid: [20, 20], containment: "parent"});		
-            
-		// listen for clicks on connections, and offer to delete connections on click.
-		//
-		jsPlumb.bind("click", function(conn, originalEvent) {
-		    if (confirm("Delete connection from " + conn.sourceId + " to " + conn.targetId + "?"))
-		    jsPlumb.detach(conn); 
-		});	
-			
-		//jsPlumb.bind("connectionDrag", function(connection) {
-		//    console.log("connection " + connection.id + " is being dragged. suspendedElement is ", connection.suspendedElement, " of type ", connection.suspendedElementType);
-		//});		
-		//	
-		//jsPlumb.bind("connectionDragStop", function(connection) {
-		//    console.log("connection " + connection.id + " was dragged");
-		//});
+			});
 		},
 
 	};
 })();
+
--- src/damis/static/js/taskBoxes.js
+++ src/damis/static/js/taskBoxes.js
@@ -1,68 +1,94 @@
-;(function() {
+;
+(function() {
 	window.taskBoxes = {
-            countBoxes : 0,
+		countBoxes: 0,
 
-            // Initialize a task box ready to accept click events
-            initTask: function(ev, ui, taskContainer) {
-                // drop the task where it was dragged
-                var taskBox = $("<div>New task</div>");
-                taskBox.appendTo(taskContainer);
-                taskBox.css("left", ui.position.left + "px");
-                taskBox.css("top", ui.position.top + "px");
+		handleTaskBoxRightClick: function(ev) {
+			if (ev.button == 2) {
+				jsPlumb.detachAllConnections(ev.target);
+				$(ev.target).find(".delete-row").click();
+				$(ev.target).remove();
+                //TODO: endpoints must also be removed; maybe their ids should
+                //be derivable from task box id
+			}
+		},
 
-                //assign id and class
-                count = window.taskBoxes.countBoxes;
-                window.taskBoxes.countBoxes++;
-                taskBox.attr("id",  "task-box-" + count);
-                taskBox.addClass("task-box");
+		// Initialize a task box ready to accept click events
+		initTask: function(ev, ui, taskContainer) {
+			// drop the task where it was dragged
+			var taskBox = $("<div>New task</div>");
+			taskBox.appendTo(taskContainer);
+			taskBox.css("left", ui.position.left + "px");
+			taskBox.css("top", ui.position.top + "px");
 
-                //create modal window with form fields
-                // click add form btn 
-                var addTaskBtn = $("a[class=\"add-row\"]")
-                addTaskBtn.click();
-                
-                // get generated form
-                var taskForm = addTaskBtn.prev();
-                taskForm.attr("id", taskBox.attr("id") + "-form");
-                taskForm.dialog({
-                    autoOpen: false,
-                    appendTo: "#task-forms",
-                    modal: true,
-                    buttons: [ 
-                       {text: "Save", click: function() { 
-                           /* TODO: create endpoints for in/out parameters */ 
-                           $( this ).dialog( "close" ); }
-                       },
-                       {text: "Cancel", click: function() { $( this ).dialog( "close" ); }}
-                    ]
-                });
-                taskBox.on('dblclick', function (ev) {
-                    var taskFormId = $(ev.currentTarget).attr("id") + "-form";
-                    $("#" + taskFormId).dialog('open'); 
-                });
+			//assign id and class
+			count = window.taskBoxes.countBoxes;
+			window.taskBoxes.countBoxes++;
+			taskBox.attr("id", "task-box-" + count);
+			taskBox.addClass("task-box");
+			taskBox.on("mousedown", this.handleTaskBoxRightClick);
 
-                //make it draggable
-                jsPlumb.draggable(taskBox, { grid: [20, 20], containment: "parent"});		
-                 
-                // TODO: remove when endpoints are rendered according to in/out
-                // parameters
-                jsPlumb.addEndpoint(taskBox, window.experimentCanvas.getSourceEndpoint(), {anchor: "LeftMiddle"});
-                jsPlumb.addEndpoint(taskBox, window.experimentCanvas.getTargetEndpoint(), {anchor: "RightMiddle"});
-            },
+			//create modal window with form fields
+			// click add form btn 
+			var addTaskBtn = $("a[class=\"add-row\"]")
+			addTaskBtn.click();
 
-            initToolBox : function(spec) {
-                $(spec.draggable).draggable({
-                    appendTo: spec.droppable,
-                    helper: "clone"
-                }); 
-                $(spec.droppable).droppable({
-                    activeClass: "active-canvas",
-                    accept: spec.draggable,
-                    drop: function (ev, ui) {
-                        window.taskBoxes.initTask(ev, ui, spec.droppable);
-                    }
-                });
-            },
-        }
+			// get generated form
+			var taskForm = addTaskBtn.prev();
+			taskForm.attr("id", taskBox.attr("id") + "-form");
+			taskForm.dialog({
+				autoOpen: false,
+				appendTo: "#task-forms",
+				modal: true,
+				buttons: [{
+					text: "Save",
+					click: function() {
+						/* TODO: create endpoints for in/out parameters */
+						$(this).dialog("close");
+					}
+				},
+				{
+					text: "Cancel",
+					click: function() {
+						$(this).dialog("close");
+					}
+				}]
+			});
+			taskBox.on('dblclick', function(ev) {
+				var taskFormId = $(ev.currentTarget).attr("id") + "-form";
+				$("#" + taskFormId).dialog('open');
+			});
+
+			//make it draggable
+			jsPlumb.draggable(taskBox, {
+				grid: [20, 20],
+				containment: "parent"
+			});
+
+			// TODO: remove when endpoints are rendered according to in/out
+			// parameters
+			jsPlumb.addEndpoint(taskBox, window.experimentCanvas.getSourceEndpoint(), {
+				anchor: "LeftMiddle"
+			});
+			jsPlumb.addEndpoint(taskBox, window.experimentCanvas.getTargetEndpoint(), {
+				anchor: "RightMiddle"
+			});
+		},
+
+		initToolBox: function(spec) {
+			$(spec.draggable).draggable({
+				appendTo: spec.droppable,
+				helper: "clone"
+			});
+			$(spec.droppable).droppable({
+				activeClass: "active-canvas",
+				accept: spec.draggable,
+				drop: function(ev, ui) {
+					window.taskBoxes.initTask(ev, ui, spec.droppable);
+				}
+			});
+		},
+	}
 
 })();
+
