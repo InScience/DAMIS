9cb79107ffe5da863bed2aec1b85a5c705354f3a
grazina <grazina.laurinaviciute@gmail.com>
2014-01-06 23:31:48 +0200

    Test fix for IE8 delete-confirm problem.

--- src/damis/static/js/utils.js
+++ src/damis/static/js/utils.js
@@ -10,25 +10,25 @@
 				var dialog = $(".delete-confirm");
 				if (!dialog.hasClass("ui-dialog-content")) {
 					dialog.dialog({
-						modal: true,
-						autoopen: false,
-						appendTo: "body",
-						buttons: [{
-							text: gettext("Cancel"),
-                            class: "btn btn-primary",
-							click: function(ev) {
+						"modal": true,
+						"autoopen": false,
+						"appendTo": "body",
+						"buttons": [{
+							"text": gettext("Cancel"),
+                            "class": "btn btn-primary",
+							"click": function(ev) {
 								$(this).dialog("close");
 							}
 						},
 						{
-							text: gettext("OK"),
-                            class: "btn",
-							click: function(ev) {
+							"text": gettext("OK"),
+                            "class": "btn",
+							"click": function(ev) {
 								$(this).dialog("close");
 								$(formSelector).submit();
 							}
 						}],
-						open: function() {
+						"open": function() {
 							var dialog = $(this).closest(".ui-dialog");
 							dialog.find(".ui-dialog-titlebar > button").remove();
 						}
