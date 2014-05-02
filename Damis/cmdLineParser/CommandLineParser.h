299d5e9c04a881f2b941d054b8118a7ba1305f9d
PovilasT <povilas.treigys@insciense.lt>
2014-05-02 16:32:01 +0300

    Mpi_x86 and commandLineParser

---
+++ Damis/cmdLineParser/CommandLineParser.h
@@ -0,0 +1,26 @@
+
+#if !defined COMMANDLINEPARSER_H
+#define COMMANDLINEPARSER_H
+
+#include <vector>
+#include <string>
+#include <map>
+class CommandLineParser
+{
+public:
+
+    CommandLineParser(int argc_, char * argv_[],bool switches_on_=false);
+    ~CommandLineParser(){}
+
+    std::string get_arg(int i);
+    std::string get_arg(std::string s);
+
+private:
+
+    int argc;
+    std::vector<std::string> argv;
+
+    bool switches_on;
+    std::map<std::string,std::string> switch_map;
+};
+#endif // !defined(COMMANDLINEPARSER_H)
